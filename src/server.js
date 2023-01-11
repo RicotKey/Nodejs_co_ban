b4ffe8e7282984c12207b4505c483cdbce7bdc90
RicotKey <keynguyen.121@gmail.com>
2023-01-11 19:23:38 +0700

    file .env + public file public in browser

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import configViewEngine from "./configs/viewEngine";
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8080;
 
 configViewEngine(app);
 
